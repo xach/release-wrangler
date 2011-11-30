3c6a4eeece0de40557b0820ae7c29efcfbf844dc
Zach Beane <xach@xach.com>
2011-11-29 21:57:42 -0500

    Rework things to be more staged.
    Failures in one stage don't have to screw up re-trying subsequent
    stages now.

--- project.lisp
+++ project.lisp
@@ -18,16 +18,29 @@
 (defgeneric commit (project new-version))
 (defgeneric tag (project new-version))
 (defgeneric push-upstream (project))
-(defgeneric publish (project new-version))
+(defgeneric publish (project))
 (defgeneric git-url (project))
 (defgeneric git-tag-string (project new-version))
+(defgeneric tags (project))
+(defgeneric latest-tag (project)
+  (:method (project)
+    (first (last (tags project)))))
+
+(defmethod checkout-directory (project)
+  (make-pathname :directory (list :absolute
+                                  :home "release-wrangler" (name project))))
 
 (defmethod git-url (project)
   (format nil "git@github.com:xach/~A.git"
           (name project)))
 
 (defmethod check-out (project)
-  (run "git" "clone" (git-url project)))
+  (ensure-directories-exist "~/release-wrangler/")
+  (with-posix-cwd "~/release-wrangler/"
+    (if (probe-file (checkout-directory project))
+        (with-posix-cwd (checkout-directory project)
+          (run "git" "pull"))
+        (run "git" "clone" (git-url project)))))
 
 (defmethod commit (project new-version)
   (run "git" "add" ".")
@@ -38,11 +51,13 @@
   (format nil "release-~A" new-version))
 
 (defmethod tag (project new-version)
-  (run "git" "tag" (git-tag-string project new-version)))
+  (in-project-directory project
+    (run "git" "tag" (git-tag-string project new-version))))
 
 (defmethod push-upstream (project)
-  (run "git" "push" "origin" "master")
-  (run "git" "push" "--tags" "origin" "master"))
+  (in-project-directory project
+    (run "git" "push" "origin" "master")
+    (run "git" "push" "--tags" "origin" "master")))
 
 (defmethod suffixed-pathname (pathname suffix)
   (values
@@ -50,52 +65,69 @@
                                   "."
                                   suffix))))
 
-(defmethod publish (project new-version)
-  (let* ((base (merge-pathnames "../"))
-         (html (merge-pathnames "../lisp/"))
-         (name (name project))
-         (tar (make-pathname :name name :type "tar"
-                             :defaults base))
-         (gz (suffixed-pathname tar "gz"))
-         (tgz (make-pathname :name name :type "tgz"
-                             :defaults base))
-         (asc (suffixed-pathname tgz "asc")))
-    (run "git" "archive"
-         :format "tar"
-         :prefix (format nil "~A-~A/" name new-version)
-         :output tar
-         (git-tag-string project new-version))
-    (run "gzip" "-v" tar)
-    (rename-file gz tgz)
-    (sign tgz)
-    (alexandria:copy-file tgz (merge-pathnames html tgz))
-    (alexandria:copy-file asc (merge-pathnames html asc))
-    (run "cp" "-a" "doc/" (concatenate 'string "../lisp/" name))
-    (with-posix-cwd "../"
-      (run "tar" "czvf" "release-wrangler.tgz" "lisp/")
-      (scp "release-wrangler.tgz"
-           :host "bradley.xach.com"
-           :remote-path "tmp/"))
-    (ssh t "bradley.xach.com"
-         "cd www.xach.com && tar xzvf ~/tmp/release-wrangler.tgz")))
+(defmacro in-project-directory (project &body body)
+  (let ((proj (copy-symbol 'project))
+        (path (copy-symbol 'path)))
+    `(let* ((,proj ,project)
+            (,path (checkout-directory ,proj)))
+       (unless (probe-file ,path)
+         (check-out ,proj))
+       (with-posix-cwd (checkout-directory ,proj)
+         ,@body))))
+
+(defmethod tags (project)
+  (in-project-directory project
+    (run-output-lines "git" "tag" "-l")))
+
+
+
+
+(defmethod publish (project)
+  (with-temporary-directory (base "~/release-wrangler/publish/")
+    (let* ((html (merge-pathnames "lisp/" base))
+           (name (name project))
+           (tar (make-pathname :name name :type "tar"
+                               :defaults base))
+           (gz (suffixed-pathname tar "gz"))
+           (tgz (make-pathname :name name :type "tgz"
+                               :defaults base))
+           (asc (suffixed-pathname tgz "asc"))
+           (version (current-version project)))
+      (ensure-directories-exist html)
+      (in-project-directory project
+        (run "git" "archive"
+             :format "tar"
+             :prefix (format nil "~A-~A/" name version)
+             :output tar
+             (git-tag-string project version))
+        (run "gzip" "-v" tar)
+        (rename-file gz tgz)
+        (sign tgz)
+        (alexandria:copy-file tgz (merge-pathnames html tgz))
+        (alexandria:copy-file asc (merge-pathnames html asc))
+        (run "cp" "-a" "doc/" (merge-pathnames name html)))
+      (with-posix-cwd base
+        (run "tar" "czvf" "release-wrangler.tgz" "lisp/")
+        (scp "release-wrangler.tgz"
+             :host "bradley.xach.com"
+             :remote-path "tmp/"))
+      (ssh t "bradley.xach.com"
+           "cd www.xach.com && tar xzvf ~/tmp/release-wrangler.tgz"))))
 
 
 (defgeneric release (project new-version)
   (:method (project new-version)
-    (in-temporary-directory (format nil "~A/" (name project))
-      (let ((bundle-base (merge-pathnames "lisp/")))
-        (ensure-directories-exist bundle-base)
-        (check-out project)
-        (with-posix-cwd (name project)
-          (let ((current-version (current-version project)))
-            (unless (member new-version
-                            (successive-versions current-version)
-                            :test 'equalp)
-              (error "Bad version transition ~A -> ~A"
-                     current-version new-version)))
-          (update-system-file project new-version)
-          (update-documentation project new-version)
-          (commit project new-version)
-          (tag project new-version)
-          (push-upstream project)
-          (publish project new-version))))))
+    (let ((path (checkout-directory project))
+          (name (name project)))
+      (check-out project)
+      (with-posix-cwd path
+        (let ((current-version (current-version project)))
+          (unless (member new-version
+                          (successive-versions current-version)
+                          :test 'equalp)
+            (error "Bad version transition ~A -> ~A"
+                   current-version new-version)))
+        (update-system-file project new-version)
+        (update-documentation project new-version)
+        (commit project new-version)
+        (tag project new-version)))))
